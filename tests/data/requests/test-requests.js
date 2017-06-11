056e2b250858538fecc3762d1e41804ad7510b45
Simon Olofsson <s.olofsson@gmail.com>
2017-06-11 19:16:01 +0200

    Cleaned up tests slightly.

--- tests/data/requests/test-requests.js
+++ tests/data/requests/test-requests.js
@@ -6,27 +6,38 @@ require('whatwg-fetch');
 
 const requests = require('../../../src/data/requests/requests');
 
+// The following test helper and stubbing of window.fetch was taken from https://rjzaworski.com/2015/06/testing-api-requests-from-window-fetch
+const jsonOk = function(body) {
+
+    let mockResponse = new window.Response(JSON.stringify(body), {
+        status: 200,
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    return Promise.resolve(mockResponse);
+
+};
+
 describe('Requests', () => {
 
-    // The stubbing of window.fetch was taken from https://rjzaworski.com/2015/06/testing-api-requests-from-window-fetch
+    const HOST = 'http://localhost:8080/backend/server.php/';
 
     beforeEach(() => {
 
         let fetchStub = sinon.stub(window, 'fetch');
 
-        let nonBodyOkItems = {
-            status: 200,
-            headers: {'Content-Type': 'application/json'}
-        };
+        fetchStub.withArgs(HOST + 'gigs', {method: 'GET'}).returns(jsonOk({
 
-        let getBody = '{"ticketlink": "http://www.hässleholmsfesten.se", "datetime": "2017-08-25 16:00:00", "venue_name": "Hässleholms sommarfest"}';
+            ticketlink: "http://www.hässleholmsfesten.se",
+            datetime: "2017-08-25 16:00:00",
+            venue_name: "Hässleholms sommarfest"
 
-        let getRes = new window.Response(getBody, nonBodyOkItems);
-        let postRes = new window.Response('{}', nonBodyOkItems);
+        }));
 
-        fetchStub.withArgs('http://localhost:8080/backend/server.php/gigs', {method: 'GET'}).returns(Promise.resolve(getRes));
-        fetchStub.withArgs(('http://localhost:8080/backend/server.php/gigs?ticketlink=http%3A%2F%2Fwww.h%C3%A4ssleholmsfesten.se&datetime=2017-08-25%2016%3A00%3A00&venue_name=H%C3%A4ssleholms%20sommarfest&authToken=mockAuthToken'),
-            {method: 'POST'}).returns(Promise.resolve(postRes));
+        fetchStub.withArgs((HOST + 'gigs?ticketlink=' + encodeURIComponent('http://www.hässleholmsfesten.se') +
+            '&datetime=' + encodeURIComponent('2017-08-25 16:00:00') +
+            '&venue_name=' + encodeURIComponent('Hässleholms sommarfest') + '&authToken=mockAuthToken'),
+         {method: 'POST'}).returns(Promise.resolve(jsonOk({})));
 
     });
 
@@ -38,7 +49,7 @@ describe('Requests', () => {
 
         it('should return OK on valid GET to /gigs', () => {
 
-            requests.getRequest('http://localhost:8080/backend/server.php/gigs', {}, {}, (fulfilled) => {
+            requests.getRequest(HOST + 'gigs', {}, {}, (fulfilled) => {
 
                 expect(fulfilled.ticketlink).to.equal("http://www.hässleholmsfesten.se");
                 expect(fulfilled.datetime).to.equal("2017-08-25 16:00:00");
@@ -58,7 +69,7 @@ describe('Requests', () => {
 
         it('should return OK on valid POST to /gigs', () => {
 
-            requests.postRequest('http://localhost:8080/backend/server.php/gigs', {
+            requests.postRequest(HOST + 'gigs', {
 
                 ticketlink: "http://www.hässleholmsfesten.se",
                 datetime: "2017-08-25 16:00:00",
